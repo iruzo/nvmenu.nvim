6fedd5a271636a8e016ae2232849ae75d0085c3b
amnesia <amnesia>
2025-10-12 22:48:09 +0000

    feat(): dynamic theme support for colorscheme changes
    Implement automatic highlight refresh when colorscheme changes to support
    themes that switch between light/dark modes based on system settings.
    
    - Add refresh_highlights() function to centralize highlight setup
    - Create ColorScheme autocmd to detect theme changes
    - Modify eventignore to be selective instead of blocking all events
    - Add proper autocmd cleanup when exiting finder

--- lua/nvmenu/init.lua
+++ lua/nvmenu/init.lua
@@ -10,7 +10,7 @@ M.config = {
   transparent_background = false
 }
 
-local function create_fuzzy_finder(process_fn)
+local function refresh_highlights()
   if M.config.transparent_background then
     vim.cmd('highlight Normal guibg=NONE ctermbg=NONE')
     vim.cmd('highlight NonText guibg=NONE ctermbg=NONE')
@@ -18,12 +18,22 @@ local function create_fuzzy_finder(process_fn)
     vim.cmd('highlight EndOfBuffer guibg=NONE ctermbg=NONE')
     vim.cmd('highlight Visual guibg=NONE ctermbg=NONE gui=reverse cterm=reverse')
   end
-  
+
   -- Create custom highlight for matched characters using theme colors
   vim.cmd('highlight default link NvmenuMatch Function')
+end
 
-  -- Disable all events to prevent plugin interference
-  vim.o.eventignore = 'all'
+local function create_fuzzy_finder(process_fn)
+  refresh_highlights()
+
+  -- Set up autocmd to refresh highlights when colorscheme changes
+  local autocmd_id = vim.api.nvim_create_autocmd("ColorScheme", {
+    callback = refresh_highlights,
+    desc = "Refresh nvmenu highlights on colorscheme change"
+  })
+
+  -- Disable specific events to prevent plugin interference while allowing ColorScheme
+  vim.o.eventignore = 'BufEnter,BufLeave,BufWinEnter,BufWinLeave,WinEnter,WinLeave'
 
   local source_buf = vim.api.nvim_get_current_buf()
   local lines = vim.api.nvim_buf_get_lines(source_buf, 0, -1, false)
@@ -57,11 +67,13 @@ local function create_fuzzy_finder(process_fn)
       vim.fn.setreg('+', text)
     end
 
+    vim.api.nvim_del_autocmd(autocmd_id)
     ui.restore_window_options(original_ui_opts)
     vim.cmd('quit!')
   end
 
   local function close_finder()
+    vim.api.nvim_del_autocmd(autocmd_id)
     ui.restore_window_options(original_ui_opts)
     vim.cmd('quit!')
   end
